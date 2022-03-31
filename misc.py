9b3e426dea314c238ddf7cdf91a20abbf23b75f4
nsde <neostyxde@gmail.com>
2022-03-31 23:37:08 +0200

    Added Dead By Daylight Code Fetcher

--- misc.py
+++ misc.py
@@ -1,7 +1,27 @@
 import flask    
 import requests
+import subprocess
 
 def register(app: flask.Flask):
+    @app.route('/dbd')
+    def dbd():
+        posts = subprocess.check_output(f'twint -u dbdcodes', shell=True).decode('utf-8')
+        scraped_posts = []
+
+        for post in posts.split('\n')[:-3]:
+            post_time = ' '.join(post.split()[1:3])
+            post_text = post.split('> ')[1].split(' https://t.co/')[0]
+            post_url = post.split()[-1]
+            post_blood = 0
+
+            if 'k Bloodpoints' in post_text:
+                post_blood = int(post_text.split(' For ')[1].split('k Bloodpoints')[0])
+
+            if not post_text.startswith('@'):
+                scraped_posts.append({'time': post_time, 'text': post_text, 'url': post_url, 'blood': '🩸'*(post_blood//10) or 'ℹ️'})
+
+        return flask.render_template('dbd.html', posts=scraped_posts)
+
     @app.route('/reload/lila.css')
     def lilacss_cdn():
         error = 'Unknown'

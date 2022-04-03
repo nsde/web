6d71428bd9376aaaeeb1d160f42b658536593f3d
nsde <neostyxde@gmail.com>
2022-04-03 23:55:31 +0200

    Navbar update

--- main.py
+++ main.py
@@ -6,15 +6,7 @@ import requests
 
 def register(app: flask.Flask):
     @app.route('/')
-    def home():
-        # status_data = requests.get('https://discord.com/api/guilds/717380468923695186/widget.json').json()
-        
-        # try:
-        #     my_status = [m for m in status_data['members'] if m['username'] == 'ONLIX'][0]
-        # except:
-        #     my_status = None
-        # repos=requests.get('https://api.github.com/users/nsde/repos').json()
-        
+    def home():        
         return flask.render_template('home.html', posts=blog.get_posts()[:3])
 
     @app.route('/red')

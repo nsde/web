44560a30cf2a8c19a85b1e39b9c0bbbf98b8dec4
nsde <neostyxde@gmail.com>
2022-05-17 23:59:25 +0200

    :sparkles: Homepage stuff & more

--- misc.py
+++ misc.py
@@ -1,13 +1,39 @@
 import tools
 
 import os
-import flask    
+import flask
+import random
 import requests
 import subprocess
 
 from werkzeug.utils import secure_filename
 
 def register(app: flask.Flask):
+    @app.route('/personify')
+    def personify():
+        return flask.render_template('personify.html', p=requests.get('https://random-data-api.com/api/users/random_user').json())
+
+    @app.route('/bored')
+    def bored():
+        no = '''
+No, that's boring, too.
+No...
+Another one...
+Nope...
+No?!
+Sorry, no...
+I don't want to do this..!
+Another one, please!
+Next one!
+Another one!
+Generate one more!
+Gimme another one, please!
+Boorinng!
+Can you show me another one, please?
+Show me another activity, please!
+'''        
+        return flask.render_template('bored.html', data=requests.get('https://www.boredapi.com/api/activity').json(), no=random.choice(no.split('\n')))
+
     @app.route('/share', methods=['GET', 'POST'])
     def share_upload():
         if flask.request.method == 'POST':
